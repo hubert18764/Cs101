4a45788c9f82f98e050da0167f55d53c78101208
hubert18764 <hubert.love1005@gmail.com>
2025-09-18 15:37:26 +0800

    Create main4.c

---
+++ EX2/main4.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main(){
+    int a = 10;
+    if (a==10){
+        printf("a is equal to 10\n");
+    }
+    else if (a<20){
+        printf("a is less than 20\n");
+    }
+    else {
+        printf("a is not less than 20\n");
+    }
+    return 0;
+}
