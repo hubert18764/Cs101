6f570fbe46eefeda05dfbbfcfca2714fd872801f
hubert18764 <hubert.love1005@gmail.com>
2025-09-18 15:25:29 +0800

    Create main1.c

---
+++ EX2/main1.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main(){
+    char a = 5;
+    printf("\t%d\n", a&1);
+    printf("\t%d\n", a&2);
+    printf("\t%d\n", a|2);
+
+    return 0;
+}
